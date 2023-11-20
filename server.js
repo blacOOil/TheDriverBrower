34c959f8fdee23da379a97a9fb011247ca1ee8fe
blacOOil <111561598+blacOOil@users.noreply.github.com>
2023-11-20 18:08:14 +0700

    Adjust-someleaderboard&server

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const hostname = 'localhost';
+const port = 3000;
+const bodyParser = require('body-parser');
+var cookieParser =  require('cookie-parser');
+const multer = require('multer');
+const path = require('path');
+const mysql = require('mysql');
+
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
+
+const con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "profile"
+})
+
+con.connect(err => {
+    if(err) throw(err);
+    else{
+        console.log("MySQL connected");
+    }
+})
+
+const queryDB = (sql) => {
+    return new Promise((resolve,reject) => {
+        // query method
+        con.query(sql, (err,result, fields) => {
+            if (err) reject(err);
+            else
+                resolve(result)
+        })
+    })
+}
+app.listen(port, hostname, () => {
+    console.log(`Server running at   http://${hostname}:${port}/register.html`);
+});
+

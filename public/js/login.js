5fbebe335b3b1f101aba23afbac472a5a4c3e2ef
blacOOil <111561598+blacOOil@users.noreply.github.com>
2023-11-24 08:46:22 +0700

    no message

--- public/js/login.js
+++ public/js/login.js
@@ -1,6 +1,6 @@
 window.onload = pageLoad;
 
-<<<<<<< HEAD
+
 function pageLoad(){
 	const queryString = window.location.search;
 	const urlParams = new URLSearchParams(queryString);
@@ -11,61 +11,4 @@ function pageLoad(){
 		document.getElementById('errordisplay').innerHTML = errorMessage;
 	}
 }
-=======
-const queryString = window.location.search;
-const urlParams = new URLSearchParams(queryString);
-const username = urlParams.get('username')
-const password = urlParams.get('password')
 
-function pageLoad() {
-    document.getElementById('input_checkpassword').onclick = TogglePassword;
-    var form = document.getElementById("myLogin");
-    form.onsubmit = function (event) {
-        event.preventDefault(); 
-        checkLogin();  
-    };
-
-    setFormValues(); 
-
-    if (username && password) {
-        form.username.disabled = true;
-        form.password.disabled = true;
-    }
-}
-
-
-function checkLogin() {
-    if (document.forms["myLogin"]["username"].value == username){
-        if (document.forms["myLogin"]["password"].value == password){
-            alert("ได้");
-            return false;
-
-        }
-        else {
-            alert("รหัสผิดพลาด");
-            return true;
-
-        }
-    }
-    else{
-        alert("Usernameผิดพลาด")
-         return true;
-
-    }
-}
-
-function TogglePassword() {
-    var x = document.getElementById("check_password");
-    var z = document.getElementById("label_changetext");
-  
-    if (x.type === "password" && z.innerHTML === "Hidden On") {
-      x.type = "text";
-      z.innerHTML = "Hidden Off";
-    } else {
-      x.type = "password";
-      z.innerHTML = "Hidden On";
-    }
-  }
-
-
->>>>>>> master
